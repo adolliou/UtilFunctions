b3851c05a6e1b5651d6bfd232662b51de6788735
Antoine Dolliou <antoine.dolliou@universite-paris-saclay.fr>
2023-02-10 17:39:51 +0100

    update suffix

--- UtilFunctions/make_movie.py
+++ UtilFunctions/make_movie.py
@@ -4,12 +4,12 @@ from glob import glob
 
 class MakeMovie:
     @staticmethod
-    def encode(frame_folder: str, path_save: str, start_frame=0, nb_frame=None, suffix="*.png"):
-        files = glob(os.path.join(frame_folder, suffix))
+    def encode(frame_folder: str, path_save: str, start_frame=0, nb_frame=None, suffix="png"):
+        files = glob(os.path.join(frame_folder, f'*.{suffix}'))
         if nb_frame is None:
             nb_frame = len(files) - start_frame
         spawnline = 'ffmpeg -r 10 -s 640:480 -start_number "' + f'{start_frame}' + '" -i "' + \
-                    os.path.join(frame_folder, '%05d.png') \
+                    os.path.join(frame_folder, f'%05d.{suffix}') \
                     + '" -vframes "' + f'{nb_frame}' + '" -c:v libx264 -vf fps=25 -pix_fmt yuv420p -y "' + \
                     path_save + '"'
         os.system(spawnline)
