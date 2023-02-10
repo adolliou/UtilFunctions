947ecc4029df65f5066cdaa9f04004aef1799b74
Antoine Dolliou <antoine.dolliou@universite-paris-saclay.fr>
2023-02-10 17:31:34 +0100

    correction

--- UtilFunctions/make_movie.py
+++ UtilFunctions/make_movie.py
@@ -8,9 +8,9 @@ class MakeMovie:
         files = glob(os.path.join(frame_folder, suffix))
         if nb_frame is None:
             nb_frame = len(files) - start_frame
-        spawnline = 'ffmpeg -r 10 -s 640:480 -start_number "' + start_frame + '" -i "' + \
+        spawnline = 'ffmpeg -r 10 -s 640:480 -start_number "' + f'{start_frame}' + '" -i "' + \
                     os.path.join(frame_folder, '%05d.png') \
-                    + '" -vframes "' + nb_frame + '" -c:v libx264 -vf fps=25 -pix_fmt yuv420p -y "' + \
+                    + '" -vframes "' + f'{nb_frame}' + '" -c:v libx264 -vf fps=25 -pix_fmt yuv420p -y "' + \
                     path_save + '"'
         os.system(spawnline)
 
