190f3a4f3052104301b020e2bae58353f0ffbe8d
Antoine Dolliou <antoine.dolliou@universite-paris-saclay.fr>
2023-02-10 17:19:46 +0100

    correction

--- UtilFunctions/make_movie.py
+++ UtilFunctions/make_movie.py
@@ -5,7 +5,7 @@ from glob import glob
 class MakeMovie:
     @staticmethod
     def encode(frame_folder: str, path_save: str, start_frame=0, nb_frame=None, suffix="*.png"):
-        files = glob.glob(os.path.join(frame_folder, suffix))
+        files = glob(os.path.join(frame_folder, suffix))
         if nb_frame is None:
             nb_frame = len(files) - start_frame
         spawnline = 'ffmpeg -r 10 -s 640:480 -start_number "' + start_frame + '" -i "' + \
