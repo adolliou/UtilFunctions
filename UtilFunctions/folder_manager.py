312097c1d5c249e761e79478725c49b080787411
Antoine Dolliou <antoine.dolliou@universite-paris-saclay.fr>
2023-03-01 16:45:19 +0100

    update

--- UtilFunctions/folder_manager.py
+++ UtilFunctions/folder_manager.py
@@ -12,7 +12,7 @@ class FolderManager(dict):
     @staticmethod
     def _assert_correct_dict(dict_input, list_needed_keys: list):
         assertion = np.array([(n in dict_input) for n in list_needed_keys], dtype="bool")
-        if assertion.sul != len(list_needed_keys):
+        if assertion.sum() != len(list_needed_keys):
             raise ValueError("Not correct dict keys in the input")
 
 
