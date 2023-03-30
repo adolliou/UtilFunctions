43b7c846bf180121a1e8809ccdfdd6bcc3886482
Antoine Dolliou <antoine.dolliou@universite-paris-saclay.fr>
2023-03-30 19:00:46 +0200

    added fitting.py 1st version

--- UtilFunctions/fitting.py
+++ UtilFunctions/fitting.py
@@ -23,8 +23,8 @@ class PlotSpectrum:
     def _get_edges(x: np.array):
 
         edges = [x[n + 1] - x[n] for n in range(len(x) - 1)]
-        edges.insert(0, x[0] - edges[0])
-        edges.append(x[-1] + edges[-1])
+        edges.insert(0, 2*x[0] - edges[0])
+        edges.append(2*x[-1] - edges[-1])
         return edges
 
     @staticmethod
