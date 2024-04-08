516371a78eb3e324f16e9ca647c45423a9d2031c
adolliou <antoine.dolliou@universite-paris-saclay.fr>
2024-04-08 11:13:49 +0200

    implemented inpainting algorithm

--- UtilFunctions/common_util.py
+++ UtilFunctions/common_util.py
@@ -91,4 +91,4 @@ class CommonUtil:
     @staticmethod
     def spatial_resolution(d_au: u.Quantity, alpha=1 * u.arcsec):
         Rsun = 6.95700E+08 * u.m
-        return (d_au - 1.004 * Rsun) * alpha.to("rad").value
+        return ((d_au - 1.004 * Rsun) * alpha.to("rad").value/(1.004 * Rsun)) * u.m
