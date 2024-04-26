0c6bb8f3e4d7396631f9ee45f7c4d0bf34e06303
adolliou <antoine.dolliou@universite-paris-saclay.fr>
2024-04-26 13:43:53 +0200

    implemented inpainting algorithm

--- UtilFunctions/share_memory.py
+++ UtilFunctions/share_memory.py
@@ -2,9 +2,10 @@ import numpy as np
 from multiprocessing.shared_memory import SharedMemory
 
 
-def gen_shmm(create=False, name=None, ndarray=None, size=0, shape=None, dtype=float):
+def gen_shmm(create=False, name=None, ndarray=None, size=0, shape=None, dtype=None):
     assert (type(ndarray) != type(None) or size != 0) or type(name) != type(None)
     assert type(ndarray) != type(None) or type(shape) != type(None)
+    dtype = ndarray.dtype if dtype is None else dtype = dtype
     size = size if type(ndarray) == type(None) else ndarray.nbytes
     shmm = SharedMemory(create=create, size=size, name=name)
     shmm_data = np.ndarray(shape=shape if type(ndarray) == type(None) else ndarray.shape,
