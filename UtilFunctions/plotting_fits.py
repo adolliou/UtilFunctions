17bab85975d2bc4dad06ab48616e8fb9c30fb26f
adolliou <antoine.dolliou@universite-paris-saclay.fr>
2024-06-25 10:05:50 +0200

    implemented inpainting algorithm

--- UtilFunctions/plotting_fits.py
+++ UtilFunctions/plotting_fits.py
@@ -61,7 +61,7 @@ class CmapUtil:
     def solohri_lya1216_color_table(path_idl3: str):
         solohri_lya1216 = CmapUtil.get_idl3(path_idl3)
         solohri_lya1216[:, 2] = solohri_lya1216[:, 0] * np.linspace(0, 1, 256)
-        return _cmap_from_rgb(*solohri_lya1216.T, 'SolO EUI HRI Lyman Alpha')
+        return CmapUtil._cmap_from_rgb(*solohri_lya1216.T, 'SolO EUI HRI Lyman Alpha')
 
     @staticmethod
     def aia_color_table(wavelength: u.angstrom, path_idl3: str):
