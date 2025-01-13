f6f5c6635cc21e2303210b3c744273ec8780eb21
adolliou <antoine.dolliou@universite-paris-saclay.fr>
2025-01-13 16:29:49 +0100

    finished regex EUI

--- UtilFunctions/Selector/selector_eui.py
+++ UtilFunctions/Selector/selector_eui.py
@@ -1,28 +1,56 @@
 import os.path
 from .selector import Selector
 from urllib.parse import urljoin
+import re
 
 
 class SelectorEui(Selector):
     default_base_url = "https://www.sidc.be/EUI/data/releases"
-    release_dict = {
-        "1.0": "202012_release_1.0",
-        "2.0": "202103_release_2.0",
-        "3.0": "202107_release_3.0",
-        "4.0": "202112_release_4.0",
-        "5.0": "202204_release_5.0",
-        "6.0": "202301_release_6.0",
 
-    }
+    def __init__(self, release=6.0, level=2, base_url=None, release_dict=None, level_dict=None):
+        """
 
-    level_dict = {
-        "1": "L1",
-        "2": "L2",
-        "3": "L3",
-    }
+        :param release: Release number (e.g. 6.0)
+        :param level: level of the data (e.g. 2)
+        :param base_url: url or path where the release folders are located. If none, then set to the default_base_url
+        :param release_dict: dict containing the names of each release folders. Change if needed
+        :param level_dict: dict containing the names of each level folders. Change if needed.
+        """
+        if release_dict is None:
+            self.release_dict = {
+                "1.0": "202012_release_1.0",
+                "2.0": "202103_release_2.0",
+                "3.0": "202107_release_3.0",
+                "4.0": "202112_release_4.0",
+                "5.0": "202204_release_5.0",
+                "6.0": "202301_release_6.0",
 
-    def __init__(self, release=6.0, level=2, base_url=None):
+            }
+        else:
+            self.release_dict = release_dict
+        if level_dict is None:
+            self.level_dict = {
+                "1": "L1",
+                "2": "L2",
+                "3": "L3",
+            }
+        else:
+            self.level_dict = level_dict
+
         if base_url is None:
             base_url = SelectorEui.default_base_url
-        url = base_url + '/' + SelectorEui.release_dict[str(release)] + '/' + SelectorEui.level_dict[str(level)]
+        url = base_url + '/' + self.release_dict[str(release)] + '/' + self.level_dict[str(level)]
         super().__init__(release_url_basis=url)
+
+    def get_regex(self):
+        self.re_filename = re.compile(r'''
+        solo
+        _(?P<level>L[123])
+        _eui
+        -(?P<instrument>(fsi|hrieuv|hrilya))
+        (?P<filter>(174|304|1216|opn))?
+        -image
+        _(?P<time>\d{8}T\d{6})
+        _(?P<version>V\d{2})
+        (?P<ext>\..*)
+        ''')
--- UtilFunctions/Selector/selector_spice.py
+++ UtilFunctions/Selector/selector_spice.py
@@ -1,10 +1,37 @@
 from selector import Selector
 import re
 
+
 class SelectorSpice(Selector):
 
-    def __init__(self):
-        super().__init__()
+    default_base_url = "https://spice.osups.universite-paris-saclay.fr/spice-data"
+
+    def __init__(self, release=5.0, level=2, base_url=None, release_dict=None, level_dict=None):
+
+        if release_dict is None:
+            self.release_dict = {
+                "1.0": "release-1.0",
+                "2.0": "release-2.0",
+                "3.0": "release-3.0",
+                "4.0": "release-4.0",
+                "5.0": "release-5.0",
+            }
+        else:
+            self.release_dict = release_dict
+
+        if level_dict is None:
+            self.level_dict = {
+                "1": "level1",
+                "2": "level2",
+            }
+        else:
+            self.level_dict = level_dict
+
+        if base_url is None:
+            base_url = SelectorSpice.default_base_url
+        url = base_url + '/' + self.release_dict[str(release)] + '/' + self.level_dict[str(level)]
+
+        super().__init__(release_url_basis=url)
 
     def get_regex(self):
         self.re_filename = re.compile(
