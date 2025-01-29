d4863caa277ff10019ea265966e40b040237eb94
dolliou <dolliou@mps.mpg.de>
2025-01-29 18:42:10 +0100

    debug

--- UtilFunctions/Selector/selector.py
+++ UtilFunctions/Selector/selector.py
@@ -70,9 +70,9 @@ class Selector:
         if file_name_str is None:
             paths_list = glob(os.path.join(path_basis, "*.fits"))
         else:
-            # breakpoint()
             paths_list =  glob(os.path.join(path_basis, file_name_str))
         if return_time_list:
+            breakpoint()
             time_list = [self._find_time_from_file(os.path.basename(l)) for l in paths_list
                          if ((".fits" in os.path.basename(l)) and (file_name_str in os.path.basename(l)))]
             return paths_list, time_list
