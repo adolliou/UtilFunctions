5ceac8825c183ff1323c07364421fb779cae437d
dolliou <dolliou@mps.mpg.de>
2025-02-03 18:06:47 +0100

    d

--- UtilFunctions/create_dict_file.py
+++ UtilFunctions/create_dict_file.py
@@ -93,7 +93,7 @@ def create_dict_file(suffix: str,  path_instrument: str=None, name_list_txt: str
     data_dict['date-avg'] = np.array(data_dict['date-avg'])
     data_dict['dsun-obs'] = np.array(data_dict['dsun-obs'])
     if len(data_dict['path']) == 0:
-        raise ValueError(f"could construct dict data at {path_instrument=}")
+        raise ValueError(f"could construct dict data at {path_instrument=} with {suffix=}")
     if verbose>0:
         print("%i FITS files added in the dict_file." % len(data_dict["path"]))
 
