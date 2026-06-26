54dc686852b7716d25c7c45dbb2ed83448cb4829
dolliou <antoine.dolliou@gmail.com>
2026-06-26 10:54:34 +0200

    corrected prepare data dict

--- UtilFunctions/prepare_data_dict.py
+++ UtilFunctions/prepare_data_dict.py
@@ -23,18 +23,20 @@ def prepare_data_dict(files, data_folder,sequence_folder_name,
     if "out_folder" in files:
         folderman["out"] = OutputFolderManager(files)["out"]
 
-    if "remove_str" in files:
-        dict_im = remove_paths_with_str(dict_file=dict_im, str_to_remove=dict_im["remove_str"])
 
-    if folderman["in"]["keyword_select"] is not None:  
-        dict_spice = select_values_in_header_keywords(dict_file=dict_spice, 
-                                                        keyword=folderman["in"]["keyword_select"], 
-                                                        values= folderman["in"]["values_select"], 
-                                                        window= folderman["in"]["window"],)
 
     dict_files = create_dict_file(path_instrument=folderman["in"]["path"], suffix=folderman["in"]["suffix"],
                                      window=folderman["in"]["window"], name_list_txt = folderman["in"]["name_list_txt"])
     dict_files = select_time_interval(dict_file=dict_files,
                                          date_start=files["date_start"], date_stop=files["date_stop"])
-    
+
+    if "remove_str" in files:
+        dict_files = remove_paths_with_str(dict_file=dict_files, str_to_remove=dict_files["remove_str"])
+
+    if folderman["in"]["keyword_select"] is not None:  
+        dict_files = select_values_in_header_keywords(dict_file=dict_files, 
+                                                        keyword=folderman["in"]["keyword_select"], 
+                                                        values= folderman["in"]["values_select"], 
+                                                        window= folderman["in"]["window"],)
+
     return dict_files, folderman
