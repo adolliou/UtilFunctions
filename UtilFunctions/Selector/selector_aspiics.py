342c6979d773960cb8d59b075e0ed9cc803a387c
dolliou <antoine.dolliou@gmail.com>
2026-09-26 19:38:03 +0200

    d

--- UtilFunctions/Selector/selector_aspiics.py
+++ UtilFunctions/Selector/selector_aspiics.py
@@ -52,7 +52,7 @@ class SelectorAspiicsMPS(Selector):
         _(?P<level>l[123])
         _(?P<time>\d{8}T\d{6})
         (?P<miliseconds>\d+)?
-        _(?P<version>[A-Z0-9])
+        _(?P<version>[A-Z0-9]{12})
         _(?P<versionnumber>v\d{2})?
        .fits
        (?P<compression>(.gz))?
